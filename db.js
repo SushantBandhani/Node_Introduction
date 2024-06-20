f17f48e457a3486624f1e2d5ce70ebbbf07fd6e6
Sushant Bandhani <sushantbandhani22@gmail.com>
2024-06-20 08:24:30 +0530

    Added db.js to connect mongodb and nodejs

---
+++ db.js
@@ -0,0 +1,27 @@
+const mongoose =require("mongoose")  //importing mongoose
+
+//Defining the URL
+const mongoURL='mongodb://localhost:27017/hotels'
+ 
+mongoose.connect(mongoURL,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+})
+
+// Get the default connection
+// Mongoose maintains a default connection object representing the MongoDb connection
+const db=mongoose.connection;   
+
+// Defining event listeners
+
+db.on('connected',()=>{
+    console.log('Connected to Mongodb Server')
+})
+db.on('error',()=>{
+    console.log('MongoDb connection error',err)
+})
+db.on('disconnected',()=>{
+    console.log('MongoDb disconnected')
+})
+
+module.exports=db;
