e517cd87669edbd6e6ec4a28a995f8d2a68e37bb
Sushant Bandhani <sushantbandhani22@gmail.com>
2024-06-30 01:17:49 +0530

    Updating server.js

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ const express=require('express')
 const db=require('./db')
 const app=express()
 const bodyParser=require('body-parser')
+const passport=require('./auth')
 require('dotenv').config()
-app.use(bodyParser.json())
+
+
+app.use(bodyParser.json()) 
+
 
 // Middleware function
 const logrequest=(req,res,next)=>{
@@ -11,25 +15,37 @@ const logrequest=(req,res,next)=>{
     next();
 }
 
+
 //To implement in all routes
 app.use(logrequest)
 
-app.get('/',function(req,res){
+
+// AUTHENTICATION
+
+
+// Initialize Passport
+app.use(passport.initialize());
+
+
+const localAuthMiddleware=passport.authenticate('local',{session:false});
+
+
+app.get('/',localAuthMiddleware,logrequest,function(req,res){
     res.send("Welcome to my hotel.. How i can help you,we have list of menus")
 })
+
 
 // Importing the router files
 const personRoutes=require('./routes/personRoutes')
 app.use('/person',personRoutes)
 
+
 //Importing the menu router files
 const menuItemRoutes = require('./routes/menuItemRoutes');
 app.use('/menu',menuItemRoutes)
-
 const PORT=process.env.PORT || 3000;
 
 
 app.listen(PORT,()=>{
     console.log('Server is listening to this port')
 })
-
