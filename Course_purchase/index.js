51bc251dfad647861bc301188b27a76688e12bea
Sushant Bandhani <sushantbandhani22@gmail.com>
2024-09-22 17:20:28 +0530

    Not major update

--- Course_purchase/index.js
+++ Course_purchase/index.js
@@ -7,7 +7,6 @@ const bodyParser=require('body-parser')
 const UserRoute=require('./routes/user')
 const adminRoute=require('./routes/admin')
 
-
 app.use(bodyParser.json())
 app.use('/admin',adminRoute)
 app.use('/user',UserRoute)
@@ -16,3 +15,4 @@ app.use('/user',UserRoute)
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`)
 })
+
