f5af1edfccbafae1fd0ee2b2428c01575f7c9578
Sushant Bandhani <sushantbandhani22@gmail.com>
2024-09-22 17:21:50 +0530

    Updated both middlewares to added Jsonwebtoken

--- Course_purchase/middleware/admin.js
+++ Course_purchase/middleware/admin.js
@@ -1,20 +1,27 @@
 const {Admin}= require('../db/index')
+const {JWT_SECRET}=require('../config')
+const jwt=require("jsonwebtoken")
 function adminMiddleware(req,res,next){
-    const username=req.headers.username
-    const password=req.headers.password;
-    Admin.findOne({
-        username:username,
-        password:password
-    }).then(function(value){
-        if(value){
-            next()
-        }
-        else{
-            res.status(403).json({
-                msg:"User does not exist"
-            })
-        }
-    })
+    try{
+        const token=req.headers.authorization;
+    const words=token.split("")
+    const jwtToken=words[1]
+    const decodedValue=jwt.verify(jwtToken,JWT_SECRET)
+    if(decodedValue.username){
+        next()
+    }
+    else{
+        res.status(403).json({
+            msg:'incorrect inputs'
+        })
+    }
+    }
+    catch(err){
+        res.status(403).json({
+            msg:"You are not authenticated"
+        })
+    }
+
 }
 
 module.exports=adminMiddleware
--- Course_purchase/middleware/user.js
+++ Course_purchase/middleware/user.js
@@ -1,21 +1,21 @@
 const {User}= require('../db/index')
+const {JWT_SECRET}=require('../config')
+
 
 function userMiddleware(req,res,next){
-    const username=req.headers.username
-    const password=req.headers.password;
-    User.findOne({
-        username:username,
-        password:password
-    }).then(function(value){
-        if(value){
-            next()
-        }
-        else{
-            res.status(403).json({
-                msg:"User does not exist"
-            })
-        }
-    })
+    const token=req.headers.authorization;
+    const words=token.split("")
+    const jwtToken=words[1]
+    const decodedValue=jwt.verify(jwtToken,JWT_SECRET)
+    if(decodedValue.username){
+        req.username=username
+        next()
+    }
+    else{
+        res.status(403).json({
+            msg:'You are not authenticated'
+        })
+    }
 }
 
 module.exports=userMiddleware;
