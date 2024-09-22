a13e4cd308d4dbeb1bfc139b2896c4175be4ba22
Sushant Bandhani <sushantbandhani22@gmail.com>
2024-09-22 14:32:11 +0530

    Setting up the index file

---
+++ Course_purchase/index.js
@@ -0,0 +1,18 @@
+const express=require('express')
+const app=express()
+const PORT=3000
+const db=require('./db/index')
+const bodyParser=require('body-parser')
+
+const UserRoute=require('./routes/user')
+const adminRoute=require('./routes/admin')
+
+
+app.use(bodyParser.json())
+app.use('/admin',adminRoute)
+app.use('/user',UserRoute)
+
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`)
+})
